0887ab2211c214878547cb8ab7c00e9e4bb70941
Mukesh Kumar <mukeshmehta2041@gmail.com>
2023-08-23 19:45:29 +0530

    db Connection

---
+++ config/dbConnect.js
@@ -0,0 +1,13 @@
+const { log } = require("console");
+const mongoose = require("mongoose");
+
+const dbConnect = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URL);
+    console.log("DB Connected Successfully");
+  } catch (error) {
+    console.log("Db Connection Failed", error.message);
+  }
+};
+
+dbConnect();
--- server.js
+++ server.js
@@ -1,5 +1,7 @@
+require("dotenv").config();
 const http = require("http");
 const app = require("./app/app");
+require("./config/dbConnect");
 
 const PORT = process.env.PORT || 2020;
 
