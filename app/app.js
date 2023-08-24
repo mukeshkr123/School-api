30c41c42dc364250d64702f0b99e94c70000d91c
Mukesh Kumar <mukeshmehta2041@gmail.com>
2023-08-24 17:19:32 +0530

    custom global error handling

--- app/app.js
+++ app/app.js
@@ -7,6 +7,22 @@ const app = express();
 app.use(express.json()); // pass incoming json data
 app.use(morgan("dev"));
 
+//Error handler
+app.use((err, req, res, next) => {
+  //status
+  //message
+  // stack
+  const stack = err.stack;
+  const message = err.message;
+  const status = err.status ? err.status : "failed";
+  const statusCode = err.statusCode ? err.statusCode : 500;
+  res.status.json({
+    status,
+    message,
+    stack,
+  });
+});
+
 //Routes
 //Admin Router
 app.use("/api/v1/admins", adminRouter);
